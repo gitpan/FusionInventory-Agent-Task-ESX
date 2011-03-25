330700f5aa3405671dfe16db1322ca3a04349dd0
FusionInventory Project <cpan@fusioninventory.org>
2011-03-25 15:41:15 +0000

    Import of FUSINV/FusionInventory-Agent-Task-ESX-1.0.0 from CPAN.
    gitpan-cpan-distribution: FusionInventory-Agent-Task-ESX
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         FUSINV/FusionInventory-Agent-Task-ESX-1.0.0.tar.gz
    gitpan-cpan-author:       FUSINV
    gitpan-cpan-maturity:     released

---
+++ lib/FusionInventory/VMware/SOAP.pm
@@ -0,0 +1,295 @@
+package FusionInventory::VMware::SOAP;
+
+use strict;
+use warnings;
+
+use XML::TreePP;
+use LWP::UserAgent;
+use HTTP::Cookies;
+
+use FusionInventory::VMware::SOAP::Host;
+
+sub new {
+    my (undef, $params) = @_;
+
+    my $self = {
+        ua => LWP::UserAgent->new(),
+        url => $params->{url},
+        debugDir => $params->{debugDir},
+    };
+
+    my $cookie = new HTTP::Cookies( ignore_discard => 1 );
+    $self->{ua}->cookie_jar( $cookie );
+
+    push @{ $self->{ua}->requests_redirectable }, 'POST';
+    $self->{ua}->agent("VMware::PoorBoySOAP/0.1 ");
+
+    $self->{tpp} = XML::TreePP->new(force_array => [qw( returnval propSet )]);
+    return bless $self;
+}
+
+
+sub _loadSOAPDump {
+    my ($self, $name) = @_;
+
+    my $content;
+    open (FILE, "<". $self->{debugDir}."/".$name.".soap") or die "failed to open ".$self->{debugDir}.'/'.$name.".soap";
+    $/ = undef;
+    $content = <FILE>;
+    close FILE;
+
+    return $content;
+}
+
+sub _storeSOAPDump {
+#    return; # DEBUG
+    my ($self, $action, $data) = @_;
+    open(FILE, ">$action.soap") or die;
+    print FILE $data;
+    close FILE
+}
+
+sub _send {
+    my ($self, $action, $name, $xmlToSend) = @_;
+
+    if ($self->{debugDir}) {
+        return $self->_loadSOAPDump ($name);
+    }
+
+
+    my $req = HTTP::Request->new(POST => $self->{url});
+    $req->content($xmlToSend);
+    $req->{_headers}->{soapaction} = "\"urn:vim25#".$action."\"";
+    $req->{_headers}->{accept} = ['text/xml', 'application/soap' ];
+    $req->{_headers}->{'content-length'} = length($xmlToSend);
+    $req->{_protocol} = 'HTTP/1.1';
+    $req->content_type('text/xml; charset=utf-8');
+
+    my $res = $self->{ua}->request($req);
+
+    if ($res->is_success) {
+        $self->_storeSOAPDump($name, $res->content);
+        return $res->content;
+    } else {
+        print STDERR $res->status_line, "\n";
+        return;
+    }
+}
+
+sub _parseAnswer {
+    my ($self, $answer) = @_;
+
+    return unless $answer;
+    undef $/;
+
+# We simplify the XML structure
+    my $pattern = '.*<\w+Response xmlns="urn:vim25">(.+)</\w+Response>.*$';
+    $answer =~ s/$pattern/$1/sg,;
+    $answer =~ s/ (xsi:|)type="[:\w]+"//sg;
+    $answer =~ s/[[:cntrl:]]//g;
+    my $tmpRef = $self->{tpp}->parse($answer);
+
+    my $ref = [];
+    foreach (@{$tmpRef->{returnval}}) {
+        if ($_->{propSet}) {
+            my %tmp;
+            foreach my $p (@{$_->{propSet}}) {
+                next unless $p->{val};
+                $tmp{$p->{name}} = $p->{val}
+            }
+            push @$ref, \%tmp;
+        } else {
+            push @$ref, $_;
+        }
+    }
+
+    return $ref;
+
+}
+
+sub connect {
+    my ($self, $login, $pw) = @_;
+
+    my $req = '<?xml version="1.0" encoding="UTF-8"?>
+   <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                     xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+   <soapenv:Body>
+<RetrieveServiceContent xmlns="urn:vim25"><_this type="ServiceInstance">ServiceInstance</_this>
+</RetrieveServiceContent></soapenv:Body></soapenv:Envelope>';
+
+
+    my $answer = $self->_send('ServiceInstance', 'ServiceInstance', $req);
+    return unless $answer;
+
+    my $serviceInstance = $self->_parseAnswer($answer);
+    return unless $serviceInstance;
+
+    if ($serviceInstance->[0]{about}{apiType} eq 'VirtualCenter') {
+        $self->{vcenter} = 1; # TODO
+        $self->{sessionManager} = "SessionManager";
+        $self->{propertyCollector} = "propertyCollector";
+    } else {
+        $self->{vcenter} = 0;
+        $self->{sessionManager} = "ha-sessionmgr";
+        $self->{propertyCollector} = "ha-property-collector";
+    }
+
+
+    $req =
+        '<?xml version="1.0" encoding="UTF-8"?>
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <soapenv:Body>
+        <Login xmlns="urn:vim25"><_this type="SessionManager">%s</_this>
+        <userName>%s</userName><password>%s</password></Login></soapenv:Body></soapenv:Envelope>';
+
+    $answer = $self->_send('Login', 'Login', sprintf($req, $self->{sessionManager}, $login, $pw));
+    return unless $answer;
+    return if $answer =~ /ServerFaultCode/m;
+
+    return $self->_parseAnswer($answer);
+
+}
+
+#sub getHostInfo {
+#    my ($self) = @_;
+#
+#
+#    my $req =
+#        '<?xml version="1.0" encoding="UTF-8"?><soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:soapenc="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" soap:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"><soap:Body><RetrieveServiceContent xmlns="urn:vim25"><_this type="ServiceInstance">ServiceInstance</_this></RetrieveServiceContent></soap:Body></soap:Envelope>';
+#
+#
+#    my $answer = $self->_send('RetrieveServiceContent', 'RetrieveServiceContent', $req);
+#    my $ref = $self->_parseAnswer($answer);
+#
+#    return $host;
+#}
+
+
+sub _getVirtualMachineList {
+    my ($self) = @_;
+
+    my $req =
+
+        '<?xml version="1.0" encoding="UTF-8"?>
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <soapenv:Body>
+        <RetrieveProperties xmlns="urn:vim25"><_this type="PropertyCollector">ha-property-collector</_this>
+        <specSet><propSet><type>VirtualMachine</type><all>0</all></propSet><objectSet><obj type="Folder">ha-folder-root</obj>
+        <skip>0</skip><selectSet xsi:type="TraversalSpec"><name>folderTraversalSpec</name><type>Folder</type><path>childEntity</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet><selectSet><name>datacenterHostTraversalSpec</name></selectSet><selectSet><name>datacenterVmTraversalSpec</name></selectSet><selectSet><name>datacenterDatastoreTraversalSpec</name></selectSet><selectSet><name>datacenterNetworkTraversalSpec</name></selectSet><selectSet><name>computeResourceRpTraversalSpec</name></selectSet><selectSet><name>computeResourceHostTraversalSpec</name></selectSet><selectSet><name>hostVmTraversalSpec</name></selectSet><selectSet><name>resourcePoolVmTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>datacenterDatastoreTraversalSpec</name><type>Datacenter</type><path>datastoreFolder</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>datacenterNetworkTraversalSpec</name><type>Datacenter</type><path>networkFolder</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>datacenterVmTraversalSpec</name><type>Datacenter</type><path>vmFolder</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>datacenterHostTraversalSpec</name><type>Datacenter</type><path>hostFolder</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>computeResourceHostTraversalSpec</name><type>ComputeResource</type><path>host</path><skip>0</skip></selectSet><selectSet xsi:type="TraversalSpec"><name>computeResourceRpTraversalSpec</name><type>ComputeResource</type><path>resourcePool</path><skip>0</skip><selectSet><name>resourcePoolTraversalSpec</name></selectSet><selectSet><name>resourcePoolVmTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>resourcePoolTraversalSpec</name><type>ResourcePool</type><path>resourcePool</path><skip>0</skip><selectSet><name>resourcePoolTraversalSpec</name></selectSet><selectSet><name>resourcePoolVmTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>hostVmTraversalSpec</name><type>HostSystem</type><path>vm</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>resourcePoolVmTraversalSpec</name><type>ResourcePool</type><path>vm</path><skip>0</skip></selectSet></objectSet></specSet></RetrieveProperties></soapenv:Body></soapenv:Envelope>
+        ';
+
+
+    my $answer = $self->_send('RetrievePropertiesVMList', 'RetrievePropertiesVMList', $req);
+    my $ref = $self->_parseAnswer($answer);
+    my @list;
+    if (ref($ref) eq 'HASH') {
+        push @list, $ref;
+    } else {
+        @list = @{$ref};
+    }
+
+    my @ids;
+    foreach (@list) {
+        push @ids, $_->{obj};
+    }
+
+    return \@ids;
+
+}
+
+sub _getVirtualMachineById {
+    my ($self, $id) = @_;
+
+    my $req = '<?xml version="1.0" encoding="UTF-8"?>
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <soapenv:Body>
+        <RetrieveProperties xmlns="urn:vim25"><_this type="PropertyCollector">%s</_this>
+        <specSet><propSet><type>VirtualMachine</type><all>1</all></propSet><objectSet><obj type="VirtualMachine">%s</obj>
+        </objectSet></specSet></RetrieveProperties></soapenv:Body></soapenv:Envelope>
+        ';
+
+    my $answer = $self->_send('RetrieveProperties', 'RetrieveProperties-VM-'.$id, sprintf($req, $self->{propertyCollector}, $id));
+    # hack to preserve  annotation / comment formating
+    $answer =~ s/\n/&#10;/gm;
+
+    my $ref = $self->_parseAnswer($answer);
+    return $ref;
+}
+
+sub getHostFullInfo {
+    my ($self, $id) = @_;
+
+    $id = 'ha-host' unless $id;
+
+    my $req = '<?xml version="1.0" encoding="UTF-8"?>
+        <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+        xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+        <soapenv:Body>
+        <RetrieveProperties xmlns="urn:vim25"><_this type="PropertyCollector">%s</_this>
+        <specSet><propSet><type>HostSystem</type><all>1</all></propSet><objectSet><obj type="HostSystem">%s</obj>
+        </objectSet></specSet></RetrieveProperties></soapenv:Body></soapenv:Envelope>
+        ';
+
+    my $answer = $self->_send('RetrieveProperties', 'getHostFullInfo', sprintf($req, $self->{propertyCollector}, $id));
+    my $ref = $self->_parseAnswer($answer);
+    my $vms = [];
+    my $machineIdList;
+    if (exists($ref->[0]{vm}{ManagedObjectReference})) { # ESX 3.5
+        if (ref($ref->[0]{vm}{ManagedObjectReference}) eq 'ARRAY') {
+            $machineIdList = $ref->[0]{vm}{ManagedObjectReference};
+        } else {
+            push @$machineIdList, $ref->[0]{vm}{ManagedObjectReference};
+        }
+    } else {
+        $machineIdList = $self->_getVirtualMachineList();
+    }
+    #$vm = $ref->[0]{vm};
+    foreach my $id (@$machineIdList) {
+        push @$vms, $self->_getVirtualMachineById($id);
+    }
+
+    my $host = FusionInventory::VMware::SOAP::Host->new($ref, $vms);
+    return $host;
+}
+
+sub getHostIds {
+    my ($self) = @_;
+
+
+    if (!$self->{vcenter}) {
+        return ['ha-host'];
+    }
+
+    my $req = '<?xml version="1.0" encoding="UTF-8"?>
+   <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
+                     xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+                     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+   <soapenv:Body>
+<RetrieveProperties xmlns="urn:vim25"><_this type="PropertyCollector">propertyCollector</_this>
+<specSet><propSet><type>HostSystem</type><all>0</all></propSet><objectSet><obj type="Folder">group-d1</obj>
+<skip>0</skip><selectSet xsi:type="TraversalSpec"><name>folderTraversalSpec</name><type>Folder</type><path>childEntity</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet><selectSet><name>datacenterHostTraversalSpec</name></selectSet><selectSet><name>datacenterVmTraversalSpec</name></selectSet><selectSet><name>datacenterDatastoreTraversalSpec</name></selectSet><selectSet><name>datacenterNetworkTraversalSpec</name></selectSet><selectSet><name>computeResourceRpTraversalSpec</name></selectSet><selectSet><name>computeResourceHostTraversalSpec</name></selectSet><selectSet><name>hostVmTraversalSpec</name></selectSet><selectSet><name>resourcePoolVmTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>datacenterDatastoreTraversalSpec</name><type>Datacenter</type><path>datastoreFolder</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>datacenterNetworkTraversalSpec</name><type>Datacenter</type><path>networkFolder</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>datacenterVmTraversalSpec</name><type>Datacenter</type><path>vmFolder</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>datacenterHostTraversalSpec</name><type>Datacenter</type><path>hostFolder</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>computeResourceHostTraversalSpec</name><type>ComputeResource</type><path>host</path><skip>0</skip></selectSet><selectSet xsi:type="TraversalSpec"><name>computeResourceRpTraversalSpec</name><type>ComputeResource</type><path>resourcePool</path><skip>0</skip><selectSet><name>resourcePoolTraversalSpec</name></selectSet><selectSet><name>resourcePoolVmTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>resourcePoolTraversalSpec</name><type>ResourcePool</type><path>resourcePool</path><skip>0</skip><selectSet><name>resourcePoolTraversalSpec</name></selectSet><selectSet><name>resourcePoolVmTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>hostVmTraversalSpec</name><type>HostSystem</type><path>vm</path><skip>0</skip><selectSet><name>folderTraversalSpec</name></selectSet></selectSet><selectSet xsi:type="TraversalSpec"><name>resourcePoolVmTraversalSpec</name><type>ResourcePool</type><path>vm</path><skip>0</skip></selectSet></objectSet></specSet></RetrieveProperties></soapenv:Body></soapenv:Envelope>';
+
+    my $answer = $self->_send('RetrieveProperties', 'getESXFullInfo', sprintf($req));
+    my $ref = $self->_parseAnswer($answer);
+
+    my @ids;
+    foreach (@$ref) {
+        push @ids, $_->{obj};
+    }
+
+    return \@ids;
+}
+
+
+
+1;
+
+
---
+++ lib/FusionInventory/VMware/SOAP/Host.pm
@@ -0,0 +1,338 @@
+package FusionInventory::VMware::SOAP::Host;
+
+use strict;
+use warnings;
+
+use FusionInventory::Agent::Tools;
+
+sub new {
+    my (undef, $hash, $vms) = @_;
+
+
+    my $self = {
+        hash => $hash,
+        vms => $vms
+    };
+
+
+    bless $self;
+}
+
+sub getArray {
+    my $h = shift;
+
+
+    if (ref($h) eq 'ARRAY') {
+        return $h;
+    } elsif ($h) {
+        return [$h];
+    } else {
+        return [];
+    }
+}
+
+sub getBootTime {
+    my ($self) = @_;
+
+    return $self->{hash}[0]{summary}{runtime}{bootTime}
+}
+
+sub getHostname {
+    my ($self) = @_;
+
+    return $self->{hash}[0]{name}
+
+}
+
+sub getBiosInfo {
+    my ($self) = @_;
+
+    my $bdate;
+    my $bversion;
+    my $smodel;
+    my $smanufacturer;
+    my $assettag;
+
+    eval {$bdate = $self->{hash}[0]{hardware}{biosInfo}{releaseDate};};
+    eval {$bversion = $self->{hash}[0]{hardware}{biosInfo}{biosVersion};};
+    eval {$smodel = $self->{hash}[0]{hardware}{systemInfo}{model};};
+    eval {$smanufacturer = $self->{hash}[0]{hardware}{systemInfo}{vendor};};
+    eval {$assettag = $self->{hash}[0]{hardware}{systemInfo}{otherIdentifyingInfo}{identifierValue};};
+
+    return {
+        BDATE => $bdate,
+              BVERSION => $bversion,
+              SMODEL => $smodel,
+              SMANUFACTURER => $smanufacturer,
+              ASSETTAG => $assettag,
+
+    }
+}
+
+sub getHardwareInfo {
+    my ($self) = @_;
+
+    my $name = $self->{hash}[0]{config}{network}{dnsConfig}{hostName};
+    my $dns = join '/', @{getArray($self->{hash}[0]{config}{network}{dnsConfig}{address})};
+    my $workgroup = $self->{hash}[0]{config}{network}{dnsConfig}{domainName};
+    my $memory = int($self->{hash}[0]{hardware}{memorySize} / (1024*1024));
+    my $uuid = $self->{hash}[0]{summary}{hardware}{uuid} || $self->{hash}[0]{hardware}{systemInfo}{uuid};
+    my $osversion = $self->{hash}[0]{summary}{config}{product}{version};
+    my $osname = $self->{hash}[0]{summary}{config}{product}{name};
+    my $oscomments = $self->{hash}[0]{summary}{config}{product}{fullName};
+    return {
+        NAME => $name,
+             DNS => $dns,
+             WORKGROUP => $workgroup, 
+             MEMORY => $memory,
+             UUID => $uuid,
+             OSVERSION => $osversion,
+             OSNAME => $osname,
+             OSCOMMENTS => $oscomments,
+    };
+}
+
+sub getCPUs {
+    my ($self) = @_;
+
+    my %cpuManufacturor = (
+            amd => 'AMD',
+            intel => 'Intel',
+            );
+
+    my $totalCore;
+    my $totalThread;
+    my $cpuEntries;
+    eval { $totalCore = $self->{hash}[0]{hardware}{cpuInfo}{numCpuCores} };
+    eval { $totalThread = $self->{hash}[0]{hardware}{cpuInfo}{numCpuThreads} };
+    eval { $cpuEntries = $self->{hash}[0]{hardware}{cpuPkg} };
+    my $ret = [];
+    foreach (@{getArray($cpuEntries)}) {
+        my $thread;
+        push @$ret, {
+            CORE => $totalCore / @{getArray($cpuEntries)},
+            MANUFACTURER =>  $cpuManufacturor{$_->{vendor}} || $_->{vendor},
+            NAME => $_->{description},
+            SPEED => int($_->{hz}/(1000*1000)),
+            THREAD => eval{$totalThread / $totalCore }
+        };
+    }
+
+
+    return $ret;
+}
+
+sub getControllers {
+    my ($self) = @_;
+
+    my $ret = [];
+
+    foreach (@{$self->{hash}[0]{hardware}{pciDevice}}) {
+
+
+        my $pciid = sprintf("%x:%x", $_->{vendorId}, $_->{deviceId});
+        my $pcisubsystemid = sprintf("%x:%x", $_->{subVendorId}, $_->{subDeviceId});
+        my $pciclass = sprintf("%x", $_->{classId});
+
+        $pcisubsystemid = '' if $pcisubsystemid =~ /^[0:]+$/;
+
+        # Workaround: sometime the pciid are odd negative number.
+        # e.g: 111d:ffff8018, ffff8086:244e, ffff8086:ffffa02c
+        foreach ($pciid, $pcisubsystemid, $pciclass) {
+                s/(\w+:)/000$1:/;
+                s/:(\w+)/:000$1/;
+                s/.*(\w{4}:).*(\w{4}).*/$1$2/g;
+        }
+        push @$ret, {
+            NAME => $_->{deviceName},
+                 MANUFACTURER => $_->{vendorName},
+                 PCICLASS => $pciclass,
+                 PCIID => $pciid,
+                 PCISUBSYSTEMID => $pcisubsystemid,
+                 PCISLOT => $_->{id},
+        };
+
+    }
+
+    return $ret;
+}
+
+sub getNetworks {
+    my ($self) = @_;
+
+    my $ret = [];
+    foreach (eval{@{getArray($self->{hash}[0]{config}{network}{pnic})}}) {
+        push @$ret, {
+                DESCRIPTION => $_->{device},
+                DRIVER => $_->{driver},
+                IPADDRESS => $_->{ip}{ipAddress},
+#            IPGATEWAY => '',
+                IPMASK => $_->{ip}{subnetMask},
+#            IPSUBNET => '',
+                MACADDR => $_->{mac},
+#            MTU => '',
+                PCISLOT => $_->{pci},
+                STATUS => $_->{ip}{ipAddress}?'Up':'Down',
+#            TYPE => '',
+#            VIRTUALDEV => '',
+#            SLAVES => '',
+#            MANAGEMENT => '',
+                SPEED => eval { $_->{spec}{linkSpeed}{speedMb} || '' },
+                };
+    }
+
+    foreach (eval{@{getArray($self->{hash}[0]{config}{network}{vnic})}}) {
+        push @$ret, {
+                DESCRIPTION => $_->{device},
+                DRIVER => $_->{driver},
+                IPADDRESS => eval{ $_->{ip}{ipAddress} },
+#            IPGATEWAY => '',
+                IPMASK => $_->{ip}{subnetMask},
+#            IPSUBNET => '',
+                MACADDR => $_->{mac},
+#            MTU => '',
+                PCISLOT => $_->{pci},
+                STATUS => $_->{ip}{ipAddress}?'Up':'Down',
+#            TYPE => '',
+                VIRTUALDEV => '1',
+#            SLAVES => '',
+#            MANAGEMENT => '',
+                SPEED => eval { $_->{spec}{linkSpeed}{speedMb} || '' },
+                };
+    }
+
+    my @vnic;
+    eval { push @vnic, $self->{hash}[0]{config}{network}{consoleVnic} if $self->{hash}[0]{config}{network}{consoleVnic}; };
+    eval { push @vnic, $self->{hash}[0]{config}{vmotion}{netConfig}{candidateVnic} if $self->{hash}[0]{config}{vmotion}{netConfig}{candidateVnic} };
+    foreach (@vnic) {
+        next if ref($_) ne 'HASH';
+
+        push @$ret, {
+                DESCRIPTION => $_->{device},
+                IPADDRESS => $_->{spec}{ip}{ipAddress},
+                IPMASK => $_->{spec}{ip}{subnetMask},
+                MACADDR => $_->{spec}{mac},
+                MTU => $_->{spec}{ip}{mtu},
+                STATUS => $_->{spec}{ip}{ipAddress}?'Up':'Down',
+                VIRTUALDEV => '1',
+                };
+    }
+
+    return $ret;
+}
+
+sub getStorages {
+    my ($self) = @_;
+
+    my $ret = [];
+    foreach my $entry (@{getArray($self->{hash}[0]{config}{storageDevice}{scsiLun})}) {
+        my $serialnumber;
+        my $size;
+
+
+        # TODO 
+        #$volumnMapping{$entry->{canonicalName}} = $entry->{deviceName};
+
+
+        foreach my $altName (@{getArray($entry->{alternateName})}) {
+            next unless ref($altName) eq 'HASH';
+            next unless $altName->{namespace};
+            next unless $altName->{data};
+            if ($altName->{namespace} eq 'SERIALNUM') {
+                $serialnumber .= $_ foreach (@{$altName->{data}});
+            }
+        }
+        if ($entry->{capacity}{blockSize} && $entry->{capacity}{block}) {
+            $size = int($entry->{capacity}{blockSize} *$entry->{capacity}{block})/1000;
+        }
+        push @$ret, {
+                DESCRIPTION => $entry->{displayName},
+                DISKSIZE => $size,
+#        INTERFACE
+                MANUFACTURER => getCanonicalManufacturer($entry->{model}) || $entry->{vendor},
+                MODEL => $entry->{model},
+                NAME => $entry->{deviceName},
+                TYPE => $entry->{deviceType},
+                SERIAL => $serialnumber,
+                FIRMWARE => $entry->{revision},
+#        SCSI_COID
+#        SCSI_CHID
+#        SCSI_UNID
+#        SCSI_LUN
+                };
+
+    }
+
+    return $ret;
+
+}
+
+sub getDrives {
+    my ($self) = @_;
+
+    my $ret = [];
+
+    foreach (@{getArray($self->{hash}[0]{config}{fileSystemVolume}{mountInfo})}) {
+        my $volumn;
+        if ($_->{volume}{type} && ($_->{volume}{type} =~ /NFS/i)) {
+            $volumn = $_->{volume}{remoteHost}.':'.$_->{volume}{remotePath};
+            # TODO
+#        } else {
+#            $volumn = $volumnMapping{$_->{volume}{extent}{diskName}}." ".$_->{volume}{extent}{partition};
+        }
+        push @$ret, {
+                SERIAL => $_->{volume}{uuid},
+                TOTAL => int (($_->{volume}{capacity} || 0) / (1000*1000)),
+                TYPE => $_->{mountInfo}{path},
+                VOLUMN => $volumn,
+                NAME => $_->{volume}{name},
+                FILESYSTEM => lc($_->{volume}{type})
+                };
+    }
+
+    return $ret;
+}
+
+sub getVirtualMachines {
+    my ($self) = @_;
+
+    my $ret = [];
+
+    foreach (@{$self->{vms}}) {
+        my $status;
+        if ($_->[0]{summary}{runtime}{powerState} eq 'poweredOn') {
+            $status = 'running';
+        } elsif ($_->[0]{summary}{runtime}{powerState} eq 'poweredOff') {
+            $status = 'off';
+        }
+
+        my @mac;
+        foreach (@{getArray($_->[0]{config}{hardware}{device})}) {
+            push @mac, $_->{macAddress} if $_->{macAddress};
+        }
+
+        if (!$status) {
+            print "Unknown status\n";
+#            print Dumper($_->[0]);
+        }
+        my $comment = eval { $_->[0]{config}{annotation} };
+# hack to preserve  annotation / comment formating
+        $comment =~ s/\n/&#10;/gm if $comment;
+
+        push @$ret, {
+            VMID => eval { $_->[0]{summary}{vm} },
+            NAME => eval { $_->[0]{name} },
+            STATUS => $status,
+            UUID => eval { $_->[0]{summary}{config}{uuid}},
+            MEMORY => eval { $_->[0]{summary}{config}{memorySizeMB} },
+            VMTYPE => 'VMware',
+            VCPU => eval { $_->[0]{summary}{config}{numCpu} },
+            MAC => join ('/', @mac),
+            COMMENT => $comment
+        };
+    }
+
+    return $ret;
+}
+
+1;
